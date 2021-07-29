de1ce3c640be8eb286b2c126e33ed30e0501a3e7
Yash Rathore <yrathore007.yr@gmail.com>
2021-07-30 02:50:42 +0530

    Play with Contolled Input Form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Setup from './tutorial/4-forms/setup/1-controlled-inputs'
+
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <Setup></Setup>
     </div>
   )
 }
--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,55 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    if(firstName && email){
+      const person = {id: new Date().getTime().toString(), firstName, email};
+
+      setPeople((people)=>{
+        return [...people, person];
+      });
+  
+      setFirstName('');
+      setEmail('');
+    }
+  }
+
+  return <>
+    <article>
+      <form className='form' onSubmit={handleSubmit}>
+        <div className="form-control"> 
+          <label htmlFor="firstName">Name : </label>
+          <input type="text" id="firstName" name="firstName" value={firstName} onChange={(e) => {setFirstName(e.target.value)}}></input>
+        </div>
+        <div className="form-control"> 
+          <label htmlFor="email">E-Mail : </label>
+          <input type="text" id="email" name="email" value={email} onChange={(e) => {setEmail(e.target.value)}}></input>
+        </div>
+
+        <button type="submit">Add Person</button>
+      </form>
+    </article>
+
+    {
+      people.map((person) => {
+        const {id, firstName, email} = person;
+
+        return (
+          <div className='item' key={id}>
+            <h4>{firstName}</h4>
+            <p>{email}</p>
+          </div>
+        );
+      })
+    }
+  </>
 };
 
 export default ControlledInputs;
